4ccb93d1f58e9d50fbb229bf7198c2b5b40398c3
Dragos Campean <dragos@sectorlabs.ro>
2022-08-12 16:49:31 +0300

    Initial commit

---
+++ test/specs/example.desktop.test.js
@@ -0,0 +1,8 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url("https://www.google.com");
+        await browser.pause(5000);
+    });
+});
+
+
