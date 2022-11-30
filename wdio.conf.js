463e5ad88c0f2d02ab8fcf135466f0ccde3cf181
dragos_ubuntu <campean_dragos@ymail.com>
2022-11-30 10:13:19 +0200

    Extend dummy repo

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,23 +1,17 @@
 exports.config = {
     specs: [
         './test/specs/suite1/*.js',
-        // './test/specs/suite2/*.js',
-        // './test/specs/suite3/*.js',
     ],
     suites: {
         s1: ['./test/specs/suite1/*.test.js'],
         s2: ['./test/specs/suite2/*.test.js'],
         s3: ['./test/specs/suite3/*.test.js'],
     },
-    exclude: [ './test/specs/suite1/*mobile*.js' ],
     maxInstances: 10,
     capabilities: [
         {
             browserName: 'chrome',
             'goog:platformName': 'desktop',
-            // specs: [
-            //     './test/specs/suite1/*.test.js'
-            // ],
             exclude: [
                 './test/specs/**/*mobile*.js'
             ],
@@ -25,7 +19,7 @@ exports.config = {
             'goog:chromeOptions': {
                 args: [
                     '--no-sandbox',
-                    // '--headless',
+                    '--headless',
                     '--disable-dev-shm-usage',
                     '--window-position=1050,210',
                     '--window-size=1366,768',
@@ -35,9 +29,6 @@ exports.config = {
         {
             browserName: 'chrome',
             'goog:platformName': 'mobile',
-            // specs: [
-            //     './test/specs/suite2/*.test.js'
-            // ],
             exclude: [
                 './test/specs/**/*desktop*.js'
             ],
@@ -47,7 +38,7 @@ exports.config = {
                     deviceName: 'iPhone 8',
                 },
                 args: [
-                    // '--headless',
+                    '--headless',
                     '--window-position=1050,210',
                     'use-mobile-user-agent',
                 ],
